e0b7e8472391ddeda662fa7eae46a7de51cef271
hjlim-creator <helim@pvamu.edu>
2025-03-14 10:37:27 -0500

    add pvamu

--- forktest.c
+++ forktest.c
@@ -17,7 +17,7 @@ int main(){
   {
      printf("child process: my pid = %d\n", getpid());
      printf("child process: my parent pid = %d\n", getppid());
-	 printf("hello world \n");
+	 printf("hello world and pvamu! \n");
      return 0;
   }
 
