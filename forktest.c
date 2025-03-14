c97ab6919f87d2bfe7633cf9f356975e955e7459
hjlim-creator <helim@pvamu.edu>
2025-03-14 10:45:38 -0500

    add hello mary kim

--- forktest.c
+++ forktest.c
@@ -17,6 +17,7 @@ int main(){
   {
      printf("child process: my pid = %d\n", getpid());
      printf("child process: my parent pid = %d\n", getppid());
+	 printf("hello Mary Kim \n");
      return 0;
   }
 
